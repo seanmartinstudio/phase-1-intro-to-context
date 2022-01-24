b83f7a2828acf02af439b16fe625eff8c83bd1c3
Sean Martin <sean@seanmartinstudio.com>
2022-01-24 14:42:08 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+// function createEmployeeRecord(array) {
+//     let obj = {}
+//     let timeInEvents = []
+//     let timeOurEvents = []
+//     // array[0] = firstName
+//     obj.familyName = familyName
+//     obj.title = title
+//     obj.payPerHour = payPerHour
+// }
+
+
+function createEmployeeRecord(array) {
+    let obj = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: new Array(),
+    timeOutEvents: new Array()
+    }
+    return obj
+}
+
+
+
+//Test Cases
+console.log("Test Case Below:")
+console.log('=>',createEmployeeRecord(['sean', 'martin', 'producer', 150]));
+
+console.log("------")
+
+console.log(obj)
+
+
+
