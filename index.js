42ec2d5909c2dfd962540887ea41871de6ea32eb
Sean Martin <sean@seanmartinstudio.com>
2022-02-01 12:33:25 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Your code here
-
 
 function createEmployeeRecord(array) {
     let obj = {
@@ -21,7 +19,7 @@ return array.map((elements) => {
 
 function createTimeInEvent(object, timeStamp) {
     let timeStampElements = timeStamp.split(' ')
-    console.log(timeStampElements)
+    // console.log(timeStampElements)
     let timeInObject = {  
     type: "TimeIn",
     hour: parseInt(timeStampElements[1]),
@@ -32,9 +30,41 @@ function createTimeInEvent(object, timeStamp) {
 }
 
 function createTimeOutEvent(object, timeStamp) {
-console.log(object)
-console.log(timeStamp)
+    let timeStampElements = timeStamp.split(' ')
+    console.log(timeStampElements)
+    let timeOutObject = {  
+        type: "TimeOut",
+        hour: parseInt(timeStampElements[1]),
+        date: timeStampElements[0]
+        }
+        object.timeOutEvents.push(timeOutObject)
+        return object
 }
+
+function hoursWorkedOnDate(object, dateWorked) {
+//Not sure if the dateWorked argument is doing anything much here? Revist.
+object.find(() => {
+    
+})
+// object.timeInEvents[0].date
+let timeIn = object.timeInEvents[0].hour
+let timeOut = object.timeOutEvents[0].hour
+let hours = timeIn - timeOut
+return Math.abs(hours) / 100
+}
+
+
+function wagesEarnedOnDate(object, dateWorked) {
+let wage = object.payPerHour
+return hoursWorkedOnDate(object) * wage
+}
+
+
+
+
+
+
+
 
 
 //Test cases
@@ -45,11 +75,11 @@ console.log(timeStamp)
 
 // console.log("Test Case for createEmployeeRecords:")
 
-// let twoArrays = [
-//     ['ashley', 'corbin', 'photog', 200],
-//     ['dan', 'keezer', 'dev', 300]
-// ]
-// console.log(createEmployeeRecords(twoArrays))
+let twoArrays = [
+    ['ashley', 'corbin', 'photog', 200],
+    ['dan', 'keezer', 'dev', 300]
+]
+console.log(createEmployeeRecords(twoArrays))
 
 let object = {
     firstName: 'dan',
@@ -62,6 +92,8 @@ let object = {
 
 let timeStamp = "2014-02-28 1400"
 
+let date = "0044-03-15"
+
 console.log("Test Case for creatTimeInEvent")
 
 console.log("=>", createTimeInEvent(object, timeStamp))
@@ -69,6 +101,14 @@ console.log("=>", createTimeInEvent(object, timeStamp))
 console.log("Test Case for createTimeOutEvent")
 
 console.log("=>", createTimeOutEvent(object, timeStamp))
+
+console.log("Test Case for function hoursWorkedOnDate")
+
+console.log("=>", hoursWorkedOnDate(object, date))
+
+console.log("Test Case for function wagesEarnedOnDate")
+
+console.log("=>", wagesEarnedOnDate(object, date))
 
 
 
